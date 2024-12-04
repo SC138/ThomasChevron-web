b18ac6be841996aa041987c09814390c49f9c03a
Thomas Chevron <chevron.thomas33@gmail.com>
2024-12-04 10:51:38 +0100

    Refactorisation du Nouvelle structure du loader et fix  responsive 1024px

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,28 +1,18 @@
-// import React from "react";
-// import styles from "./Loader.module.css";
-// import animation from "../../assets/animation/Animation.gif";
-
-// const Loader = () => {
-//   return (
-//     <div className={styles.loader}>
-//       <img src={animation} alt="Chargement en cours" />
-//     </div>
-//   );
-// };
-
-
-// export default Loader;
-
 import React from "react";
 import styles from "./Loader.module.css";
 
 const Loader = () => {
   return (
     <div className={styles.loader}>
-      <div className={styles.cup}>
-        <div className={styles["cup-handle"]}></div>
+      <div className={styles.cupContainer}>
+        <div className={styles.cup}>
+          <div className={styles["cup-handle"]}></div>
+        </div>
+        <div className={styles.base}></div>
+        <div className={styles.textContainer}>
+          <p className={styles.text}>Chargement...</p>
+        </div>
       </div>
-      
     </div>
   );
 };
